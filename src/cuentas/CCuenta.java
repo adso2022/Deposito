e4906ac6f320c21ff40e0e0fde0691bfa052e677
adso2022 <100096391+adso2022@users.noreply.github.com>
2022-02-21 20:04:15 +0100

    Update CCuenta.java

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,8 +1,6 @@
 package cuentas;
 
 public class CCuenta {
-
-
     private String nombre;
     private String cuenta;
     private double saldo;
@@ -14,9 +12,9 @@ public class CCuenta {
 
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
     }
 
     public double estado()
@@ -26,7 +24,7 @@ public class CCuenta {
 
     public void ingresar(double cantidad) throws Exception
     {
-        if (cantidad<0)
+        if (cantidad < 0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
